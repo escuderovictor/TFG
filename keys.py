22abb363048fb1a3228080ea748f82900c1edcea
escuderovictor <escuderovictor5@gmail.com>
2022-04-26 19:13:07 +0200

    codigo comentado

--- keys.py
+++ keys.py
@@ -9,15 +9,12 @@ access_token_secret = "i1dGHsWynzkLl1GyFfCnXc70J3dxqwBkZccblytN2F5uW"
 
 callback_uri = 'oob'
 
-filter_words = ['Elden ring', 'eldenring', 'from software', 'tren']
+filter_words = ['Elden ring', 'eldenring', 'from software', 'ps5']
 filter_languages = ['es']
 
 index_name = "tweets"
 # elastic_host = {'host': 'localhost', 'port': 9200}
 elastic_host = 'http://elastic:JvZpQ9D9JBMrza7KyHbW@localhost:9200'
-
-twitter_profile = "ELDENRING"
-
 
 
 
